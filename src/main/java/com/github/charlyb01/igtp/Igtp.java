8f84704f51b1a21963e52c88b78f9415f6f0f775
Charly B <charly5bollinger@gmail.com>
2021-12-17 12:13:35 +0100

    Added dependencies and infos
    Cloth config and mod menu integration. Added mod updater link

--- src/main/java/com/github/charlyb01/igtp/Igtp.java
+++ src/main/java/com/github/charlyb01/igtp/Igtp.java
@@ -1,4 +1,13 @@
 package com.github.charlyb01.igtp;
 
-public class Igtp {
+import com.github.charlyb01.igtp.config.ModConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
+import net.fabricmc.api.ModInitializer;
+
+public class Igtp implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
+    }
 }
--- src/main/java/com/github/charlyb01/igtp/client/IgtpClient.java
+++ src/main/java/com/github/charlyb01/igtp/client/IgtpClient.java
@@ -1,8 +1,13 @@
 package com.github.charlyb01.igtp.client;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 
 @Environment(EnvType.CLIENT)
-public class IgtpClient {
+public class IgtpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
 }
